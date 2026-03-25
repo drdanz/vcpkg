abadb164ab87328f0227b5c8e35bdc4a82ec2a6f
Daniele E. Domenichelli <daniele.domenichelli@kdab.com>
2026-03-25 18:28:04 +0100

    [kf6xmlgui] New port -- Depends on kf6config kf6configwidgets kf6coreaddons kf6globalaccel kf6guiaddons kf6i18n kf6iconthemes kf6itemviews kf6widgetsaddons

---
+++ ports/kf6xmlgui/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kxmlgui
+    REF "v${VERSION}"
+    SHA512 97214799667a88aa139fd948ad160ed1f6cc279322f34afc060c6b2e9acf54dd9fd7036dcdbf29cf790b3dd51a71254dddbfcb371f273851a43040f1dd6144a4
+    HEAD_REF master
+)
+
+# Prevent KDEClangFormat from writing to source effectively blocking parallel configure
+file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        designerplugin BUILD_DESIGNERPLUGIN
+    INVERTED_FEATURES
+        translations   KF_SKIP_PO_PROCESSING
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DBUILD_PYTHON_BINDINGS=OFF
+        -DKDE_INSTALL_PLUGINDIR=plugins
+        -DKDE_INSTALL_QTPLUGINDIR=plugins
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF6XmlGui)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
+vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
